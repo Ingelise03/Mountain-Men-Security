f55b8fa06b9fd730a6b7ef29bb30e2272779e1bb
Ingelise03 <ingelisewessels03@gmail.com>
2023-02-01 18:06:42 +0200

    Changes to be committed: 	modified:   mountainmensecurity/member/firebase.js

--- mountainmensecurity/member/firebase.js
+++ mountainmensecurity/member/firebase.js
@@ -26,7 +26,7 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.GOOGLEAPI,
+  apiKey: process.env.NEXT_PUBLIC_GOOGLEAPI ,
   authDomain: "mountainmenauth.firebaseapp.com",
   projectId: "mountainmenauth",
   storageBucket: "mountainmenauth.appspot.com",
