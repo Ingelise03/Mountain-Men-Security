f0e88b3066912072dcb026dee1761551cfedb4a3
Ingelise03 <ingelisewessels03@gmail.com>
2023-02-09 12:40:43 +0200

    Changes to be committed: 	modified:   mountainmensecurity/lib/client.js

--- mountainmensecurity/lib/client.js
+++ mountainmensecurity/lib/client.js
@@ -5,7 +5,8 @@ export const client = sanityClient({
     dataset: 'production',
     apiVersion: '2022-10-28',
     useCdn: true,
-    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+    token: skomkrpvia8rHyW0HS9HT1U3ZIAUFS8TiA9meGvah4mEStWYeHxEGAbWJufHYUaYuBONbl3KznzV1OZEEcW9dcaANKeHf00tQznS7b8KwBG6trHLBrBXJcAX3mJfJwWHhhSDFWsWrhV2isMXabSEoM0XEVopGum3ALOhGXeaH33jcSo8T8Rm
+
   });
   const builder = imageUrlBuilder(client);
   client
